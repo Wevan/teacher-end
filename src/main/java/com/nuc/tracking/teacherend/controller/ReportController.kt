aba173180ad4fa55b0467972c526db3dd49914ae
Wevan <438864121@qq.com>
2019-04-17 21:38:49 +0800

    :four_leaf_clover: 出报告文件下载完成，基本功能完成

--- src/main/java/com/nuc/tracking/teacherend/controller/ReportController.kt
+++ src/main/java/com/nuc/tracking/teacherend/controller/ReportController.kt
@@ -3,6 +3,11 @@ package com.nuc.tracking.teacherend.controller
 import com.google.common.collect.Maps
 import com.nuc.tracking.teacherend.util.HtmlToPdf
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
@@ -16,20 +21,33 @@ class ReportController {
     private lateinit var restTemplate: RestTemplate
 
     @GetMapping("/file")
-    fun report() {
-        var url = "http://localhost:8085/demo?id=1713010101&type=2&courseId=9"
-        val s = restTemplate.getForObject<String>(url)
-        var file = File("E:/a/s.html")
-        if (!file.exists()) {
-            val parent = file.parent
-            File(parent).apply {
-                this.mkdir()
-            }
-        }
+    fun report() : ResponseEntity<InputStreamResource>{
+//        var url = "http://localhost:8085/demo?id=1713010101&type=2&courseId=9"
+//        val s = restTemplate.getForObject<String>(url)
+//        var file = File("E:/a/s.html")
+//        if (!file.exists()) {
+//            val parent = file.parent
+//            File(parent).apply {
+//                this.mkdir()
+//            }
+//        }
+//
+//        file.writeText(s!!)
+//
+//        var down = HtmlToPdf.tomPdf("E:/a/s.html", "E:/a/s.pdf")
+        var filePath="E:/a/s.pdf"
+        val fileDown = FileSystemResource(filePath)
+        val headers = HttpHeaders()
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate")
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", fileDown.filename))
+        headers.add("Pragma", "no-cache")
+        headers.add("Expires", "0")
 
-        file.writeText(s!!)
-
-        var down = HtmlToPdf.tomPdf("E:/a/s.html", "E:/a/s.pdf")
-        var msg = ""
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .contentLength(fileDown.contentLength())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(InputStreamResource(fileDown.inputStream))
     }
 }
